5005be32f01b8a9777990aa2aaa6fe66023b4153
roznik-navisys <roznik@navisys.cz>
2022-02-19 10:50:52 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+	<div class="bg-yellow-500 p-8 text-center text-4xl uppercase font-medium text-white">
+		Wish List
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
